1b9341d29fdf7982d1b7a45b59782b24ed03bd46
username <useremail>
2024-07-08 13:57:37 -0700

    Second Commit Working Game

--- GuessingGame/GuessingGame.py
+++ GuessingGame/GuessingGame.py
@@ -3,31 +3,31 @@ import random
 
 
 def guessing_game():
-    print("Guess the number")
-    limit = input("Enter the limit: ")
-    print("I'm thinking of a number from 1 to {limit}")
-    random_number = random(1, limit)
-    tries = 0
+    print("Guess the number!")
+    limit = int(input("Enter the limit: "))
+    print(f"I'm thinking of a number from 1 to {limit}")
+    random_number = random.randint(1, limit)
     guess = None
+    tries = 0
     
-    While guess != random_number:
-        guess = input("Guess the number: ")
+    while guess != random_number:
+        guess = int(input("Guess the number: "))
         tries += 1
         if guess < random_number:
             print("Too low.")
         elif guess > random_number:
             print("Too high.")
             
-    print("You guessed it in {tries} tries.")
+    print(f"You guessed it in {tries} tries.")
     
 def main():
     play_again = "y"
     while play_again.lower() == "y":
         guessing_game()
-        play_again = input("Wouuld you like to play again? (y/n): ")
+        play_again = input("Would you like to play again? (y/n): ")
     if play_again.lower() == "n":
         Print("Bye!")
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
     
